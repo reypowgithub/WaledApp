c9c1147aba28e7c7424af9d330171fe8dff962ba
reypowgithub <reypowofc@gmail.com>
2025-05-19 09:28:51 +0700

    feat : menambahkan nativeWind dan image picker

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <View className="flex-row items-center gap-4">
+        <Image
+          source={{
+            uri: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fcitizen6%2Fread%2F5505737%2Fprofil-dan-agama-chelsea-islan-artis-cantik-blasteran-indonesia-amerika-serikat&psig=AOvVaw0yiVdqJa6RCkw1-cGwL_94&ust=1747707954956000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDk9dC9ro0DFQAAAAAdAAAAABAE",
+          }}
+          className="w-20 h-20"
+        ></Image>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
